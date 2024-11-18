3b6f9529b45d4f1a36b45fba51b9cc437e15e7a9
EstefanyMoE <emontano@contentoh.com>
2024-11-18 10:05:26 -0600

    feat: :sparkles: add components and backend

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+};
